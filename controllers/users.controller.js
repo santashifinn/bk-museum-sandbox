9aa217e3ebb7e95b7fce67645b42a27ac807b238
Pip Murphy <santashi.finn@gmail.com>
2025-05-01 21:44:05 +0100

    removing sign in controller to check for errors

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -31,7 +31,6 @@ exports.createUser = (req, res, next) => {
       .status(400)
       .send({ message: "Please enter username, email and password." });
   }
-
   bcrypt.hash(password, 10).then((password_hashed) => {
     addUser(username, email, password_hashed)
       .then((username) => {
@@ -42,37 +41,3 @@ exports.createUser = (req, res, next) => {
       .catch(next);
   });
 };
-
-// exports.confirmUser = (req, res, next) => {
-//   if (!req.body.username || !req.body.password) {
-//     res
-//       .status(400)
-//       .send({ message: "Please enter your username and password." });
-//   }
-
-//   selectUserByUsername(req.body.username)
-//     .then((user) => {
-//       if (user) {
-//         bcrypt.compare(req.body.password, user.hash_password, (err, result) => {
-//           if (err) {
-//             res.status(500).send({ message: "Error comparing passwords" });
-//             return;
-//           }
-//           if (result) {
-//             res
-//               .status(200)
-//               .send({ message: "Login successful.", user: { _id, username } });
-//           } else {
-//             res.status(401).send({ message: "Passwords don't match." });
-//           }
-//         });
-//       } else {
-//         res.status(400).send({ message: "User not found. Please sign up." });
-//       }
-//     })
-//     .catch((error) => {
-//       res.status(400).send({
-//         message: "There was an error while authenticating your details.",
-//       });
-//     });
-// };
