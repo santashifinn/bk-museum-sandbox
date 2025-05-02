dcbdc37a2814722f307c420c92749205313b1e5c
Pip Murphy <santashi.finn@gmail.com>
2025-05-02 14:21:26 +0100

    working on login persistance

--- db/data/test-data/faves.js
+++ db/data/test-data/faves.js
@@ -7,4 +7,12 @@ module.exports = [
     username: "bob",
     work_id: "NG-NM-7687",
   },
+  {
+    username: "amber",
+    work_id: "SK-C-211",
+  },
+  {
+    username: "amber",
+    work_id: "NG-NM-7687",
+  },
 ];
